3ef2bf72095057e232590ae879f68885ed738499
Kinqaaa <kinga.ja03@gmail.com>
2025-12-05 13:19:41 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,23 +1,6 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "c56c8b15-6f9a-4063-bb15-a4b43fd76fcf",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "",
-   "name": ""
-  },
-  "language_info": {
-   "name": ""
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from Bio.seq import Seq
+def reverse_complement(seq_str)
+ seq=Seq(seq_str)
+ return str(seq.reverse_complement())
+if __name=='__main__':
+ print(reverse_complement('ATGCATGC'))
