1d88770545ef7a4efe77ebfaaf13be2cd64477a5
Kinga Janas <kinga.ja03@gmail.com>
2025-12-05 13:13:10 +0100

    first commit

---
+++ parser.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c56c8b15-6f9a-4063-bb15-a4b43fd76fcf",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
